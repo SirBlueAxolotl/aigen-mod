3e5b617b5ec2518275b16a3f5d028e7375be4cb2
DevSBA <mansur.tallyn@gmail.com>
2025-06-15 16:15:04 -0400

    Initial commit

---
+++ src/main/java/net/sba/aigenmod/AigenMod.java
@@ -0,0 +1,16 @@
+package net.sba.aigenmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AigenMod implements ModInitializer {
+	public static final String MOD_ID = "aigen-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/sba/aigenmod/AigenModClient.java
@@ -0,0 +1,10 @@
+package net.sba.aigenmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AigenModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
