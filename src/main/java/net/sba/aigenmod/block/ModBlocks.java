1d040a089af4a767a499ebaf4dca2275283b5e1a
DevSBA <mansur.tallyn@gmail.com>
2025-06-15 17:39:45 -0400

    banana

---
+++ src/main/java/net/sba/aigenmod/block/ModBlocks.java
@@ -0,0 +1,39 @@
+package net.sba.aigenmod.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.sound.BlockSoundGroup;
+import net.sba.aigenmod.AigenMod;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+    public static final Block BANANA_BLOCK = registerBlock("banana_block",
+            new Block(AbstractBlock.Settings.create().strength(2f)
+                    .requiresTool().sounds(BlockSoundGroup.SLIME)));
+
+
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(AigenMod.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(AigenMod.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        AigenMod.LOGGER.info("Registering Mod Blocks for " + AigenMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
+            entries.add(ModBlocks.BANANA_BLOCK);
+        });
+    }
+}
